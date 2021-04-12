e104fb2228e79ac698f95622dff8f6a79e54e1e4
zambranoconz <riczam2797@gmail.com>
2021-04-12 11:28:42 -0500

    Boton de inicio de Sesion no hace la jalacion

--- app/src/main/java/mx/itesm/imssc/ontas/MainActivity.kt
+++ app/src/main/java/mx/itesm/imssc/ontas/MainActivity.kt
@@ -2,12 +2,31 @@
 
 package mx.itesm.imssc.ontas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: MainActivity
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun botonInicio(v: View){
+        configurarBotonInicioSesion()
+    }
+
+    private fun configurarBotonInicioSesion(){
+
+        binding.btnInicioSesion.setOnClickListener{
+            val intInicioSesion = Intent(baseContext, MenuPrincipal::class.java)
+            startActivity(intInicioSesion)
+        }
+    }
+
 }
---
+++ app/src/main/java/mx/itesm/imssc/ontas/MenuPrincipal.kt
@@ -0,0 +1,11 @@
+package mx.itesm.imssc.ontas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuPrincipal : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_principal)
+    }
+}
