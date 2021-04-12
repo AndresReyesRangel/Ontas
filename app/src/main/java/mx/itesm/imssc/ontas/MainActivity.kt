13287a14090618377841194afb8ba48a0e7b71eb
Andres R <A01746592@itesm.mx>
2021-04-12 13:27:02 -0500

    Pantalla inicio de sesión con todos sus componentes

--- app/src/main/java/mx/itesm/imssc/ontas/MainActivity.kt
+++ app/src/main/java/mx/itesm/imssc/ontas/MainActivity.kt
@@ -6,7 +6,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import kotlinx.android.synthetic.main.activity_main.*
 import mx.itesm.imssc.ontas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
