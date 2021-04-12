a7a66c71625178a4db6ae4152f0221af638cea68
zambranoconz <riczam2797@gmail.com>
2021-04-12 11:41:10 -0500

    El boton de inicio ya hace la jalacion

--- app/src/main/java/mx/itesm/imssc/ontas/MainActivity.kt
+++ app/src/main/java/mx/itesm/imssc/ontas/MainActivity.kt
@@ -7,14 +7,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import mx.itesm.imssc.ontas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: MainActivity
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
     fun botonInicio(v: View){
